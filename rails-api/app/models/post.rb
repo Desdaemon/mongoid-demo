6350d9affc2c90815f0cf82f56dc6a16bb7c7c7d
Viet Dinh <desdaemon@pop-os.localdomain>
2020-12-26 10:23:21 +0700

    hidden-definitions

--- rails-api/app/models/post.rb
+++ rails-api/app/models/post.rb
@@ -1,14 +1,9 @@
+# typed: true
+
 class Post
-  # @!parse extend ModelBase
   include Mongoid::Document
-
-  # @!attribute title
-  #   @return [String]
+  
   field :title, type: String
-
-  # @!attribute body
-  #   @return [String]
   field :body, type: String
-
   has_many :comments, dependent: :destroy
 end
